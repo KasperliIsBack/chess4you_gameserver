138c60be36fb1cb7b850fe1111f54ca29c7eedd2
Andri Pieren <andri.pieren>
2019-06-12 11:06:18 +0200

    made gameserver/service/turnservice/TestsTurnService.java typesafe

--- gameserver/src/test/java/com/chess4you/gameserver/service/turnservice/TestsTurnService.java
+++ gameserver/src/test/java/com/chess4you/gameserver/service/turnservice/TestsTurnService.java
@@ -13,6 +13,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Dictionary;
 import java.util.Hashtable;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -25,23 +26,23 @@ public class TestsTurnService {
     @Test
     public void TestMovementsPawn() {
         // init used objects
-        var pawn = new Pawn(Color.Black);
+        Pawn pawn = new Pawn(Color.Black);
         pawn.setPosition(new Position(2, 1));
-        var opponentPawnOne = new Pawn(Color.White);
+        Pawn opponentPawnOne = new Pawn(Color.White);
         opponentPawnOne.setPosition(new Position(2, 2));
-        var opponentPawnTwo = new Pawn(Color.White);
+        Pawn opponentPawnTwo = new Pawn(Color.White);
         opponentPawnTwo.setPosition(new Position(3, 1));
 
-        var dicPosPiece = new Hashtable();
+        Dictionary<Position, Piece> dicPosPiece = new Hashtable<>();
         dicPosPiece.put(pawn.getPosition(), pawn);
         dicPosPiece.put(opponentPawnOne.getPosition(), opponentPawnOne);
         dicPosPiece.put(opponentPawnTwo.getPosition(), opponentPawnTwo);
 
-        var arrayOfMovementsExpected = new Movement[1];
+        Movement[] arrayOfMovementsExpected = new Movement[1];
         arrayOfMovementsExpected[0] = new Movement(new Position(3, 1), pawn.getPosition(), Direction.FREnPasse);
 
         // get data for testList
-        var arrayOfMovementActual = turnService.getPossibleTurnFor(dicPosPiece, pawn);
+        Movement[] arrayOfMovementActual = turnService.getPossibleTurnFor(dicPosPiece, pawn);
 
         Assert.assertThat(arrayOfMovementActual.length, is(arrayOfMovementsExpected.length));
         AssertThatAllMovementsAreEqual(arrayOfMovementsExpected, arrayOfMovementActual);
@@ -50,16 +51,16 @@ public class TestsTurnService {
     @Test
     public void TestMovementsRock() {
         // init used objects
-        var rock = new Rock(Color.Black);
+        Rock rock = new Rock(Color.Black);
         rock.setPosition(new Position(0, 0));
-        var opponentPawnOne = new Rock(Color.White);
+        Rock opponentPawnOne = new Rock(Color.White);
         opponentPawnOne.setPosition(new Position(7, 0));
 
-        var dicPosPiece = new Hashtable();
+        Dictionary<Position, Piece> dicPosPiece = new Hashtable<>();
         dicPosPiece.put(rock.getPosition(), rock);
         dicPosPiece.put(opponentPawnOne.getPosition(), opponentPawnOne);
 
-        var arrayOfMovementsExpected = new Movement[14];
+        Movement[] arrayOfMovementsExpected = new Movement[14];
         arrayOfMovementsExpected[0] = new Movement(new Position(0, 1), rock.getPosition(), Direction.Forward);
         arrayOfMovementsExpected[1] = new Movement(new Position(0, 2), rock.getPosition(), Direction.Forward);
         arrayOfMovementsExpected[2] = new Movement(new Position(0, 3), rock.getPosition(), Direction.Forward);
@@ -76,7 +77,7 @@ public class TestsTurnService {
         arrayOfMovementsExpected[13] = new Movement(new Position(7, 0), rock.getPosition(), Direction.Right);
 
         // get data for testList
-        var arrayOfMovementActual = turnService.getPossibleTurnFor(dicPosPiece, rock);
+        Movement[] arrayOfMovementActual = turnService.getPossibleTurnFor(dicPosPiece, rock);
 
         Assert.assertThat(arrayOfMovementActual.length, is(arrayOfMovementsExpected.length));
         AssertThatAllMovementsAreEqual(arrayOfMovementsExpected, arrayOfMovementActual);
@@ -113,16 +114,16 @@ public class TestsTurnService {
     @Test
     public void TestMovementsQueen() {
         // init used objects
-        var queen = new Queen(Color.Black);
+        Queen queen = new Queen(Color.Black);
         queen.setPosition(new Position(0, 0));
-        var opponentPawnOne = new Rock(Color.White);
+        Rock opponentPawnOne = new Rock(Color.White);
         opponentPawnOne.setPosition(new Position(7, 7));
 
-        var dicPosPiece = new Hashtable();
+        Dictionary<Position, Piece> dicPosPiece = new Hashtable<>();
         dicPosPiece.put(queen.getPosition(), queen);
         dicPosPiece.put(opponentPawnOne.getPosition(), opponentPawnOne);
 
-        var arrayOfMovementsExpected = new Movement[21];
+        Movement[] arrayOfMovementsExpected = new Movement[21];
         arrayOfMovementsExpected[0] = new Movement(new Position(1, 1), queen.getPosition(), Direction.FRDiagonal);
         arrayOfMovementsExpected[1] = new Movement(new Position(2, 2), queen.getPosition(), Direction.FRDiagonal);
         arrayOfMovementsExpected[2] = new Movement(new Position(3, 3), queen.getPosition(), Direction.FRDiagonal);
@@ -146,7 +147,7 @@ public class TestsTurnService {
         arrayOfMovementsExpected[20] = new Movement(new Position(7, 0), queen.getPosition(), Direction.Right);
 
         // get data for testList
-        var arrayOfMovementActual = turnService.getPossibleTurnFor(dicPosPiece, queen);
+        Movement[] arrayOfMovementActual = turnService.getPossibleTurnFor(dicPosPiece, queen);
 
         Assert.assertThat(arrayOfMovementActual.length, is(arrayOfMovementsExpected.length));
         AssertThatAllMovementsAreEqual(arrayOfMovementsExpected, arrayOfMovementActual);
@@ -155,22 +156,22 @@ public class TestsTurnService {
     @Test
     public void TestMovementsKing() {
         // init used objects
-        var king = new King(Color.Black);
+        King king = new King(Color.Black);
         king.setPosition(new Position(0, 0));
-        var opponentPawnOne = new Rock(Color.White);
+        Rock opponentPawnOne = new Rock(Color.White);
         opponentPawnOne.setPosition(new Position(7, 7));
 
-        var dicPosPiece = new Hashtable();
+        Dictionary<Position, Piece> dicPosPiece = new Hashtable<>();
         dicPosPiece.put(king.getPosition(), king);
         dicPosPiece.put(opponentPawnOne.getPosition(), opponentPawnOne);
 
-        var arrayOfMovementsExpected = new Movement[3];
+        Movement[] arrayOfMovementsExpected = new Movement[3];
         arrayOfMovementsExpected[0] = new Movement(new Position(1, 1), king.getPosition(), Direction.FRDiagonal);
         arrayOfMovementsExpected[1] = new Movement(new Position(0, 1), king.getPosition(), Direction.Forward);
         arrayOfMovementsExpected[2] = new Movement(new Position(1, 0), king.getPosition(), Direction.Right);
 
         // get data for testList
-        var arrayOfMovementActual = turnService.getPossibleTurnFor(dicPosPiece, king);
+        Movement[] arrayOfMovementActual = turnService.getPossibleTurnFor(dicPosPiece, king);
 
         Assert.assertThat(arrayOfMovementActual.length, is(arrayOfMovementsExpected.length));
         AssertThatAllMovementsAreEqual(arrayOfMovementsExpected, arrayOfMovementActual);
