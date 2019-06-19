856073f50850191bb66ec5737d269128e56a81cd
Andri Pieren <andri.pieren>
2019-06-19 14:00:20 +0200

    remove logger

--- gameserver/src/main/java/com/chess4you/gameserver/service/GameServerService.java
+++ gameserver/src/main/java/com/chess4you/gameserver/service/GameServerService.java
@@ -2,9 +2,6 @@ package com.chess4you.gameserver.service;
 
 import com.chess4you.gameserver.data.GameServer;
 import com.chess4you.gameserver.repository.IGameServerRepository;
-import com.google.gson.Gson;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +10,6 @@ import java.util.Date;
 @Service
 public class GameServerService {
     private IGameServerRepository gameServerRepository;
-    Logger logger = LoggerFactory.getLogger(GameServerService.class);
     private final String host = "172.16.1.198";
     private final int port = 8081;
 
@@ -26,6 +22,5 @@ public class GameServerService {
         String serverName = "GameServer" + new Date().toString();
         GameServer gameServer = new GameServer(serverName, host, port);
         gameServerRepository.insert(gameServer);
-        logger.info("Create GameServe: " + new Gson().toJson(gameServer));
     }
 }
